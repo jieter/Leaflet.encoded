c137e4268deb8067161e1ccdc72f04eee028a859
Jan Pieter Waagmeester <jieter@jieter.nl>
2013-08-29 12:50:48 +0200

    export as node module

--- Polyline.encoded.js
+++ Polyline.encoded.js
@@ -15,113 +15,136 @@
 /*global L:true, console:true*/
 
 
-// Inject functionality into Leaflet
-(function (L) {
-	if (!(L.Polyline.prototype.fromEncoded)) {
-		L.Polyline.fromEncoded = function (encoded, options) {
-			return new L.Polyline(L.PolylineUtil.decode(encoded), options);
-		};
-	}
-	if (!(L.Polygon.prototype.fromEncoded)) {
-		L.Polygon.fromEncoded = function (encoded, options) {
-			return new L.Polygon(L.PolylineUtil.decode(encoded), options);
-		};
-	}
+(function () {
+	// This function is very similar to Google's, but I added
+	// some stuff to deal with the double slash issue.
+	var encodeNumber = function (num) {
+		var encodeString = "";
+		var nextValue, finalValue;
+		while (num >= 0x20) {
+			nextValue = (0x20 | (num & 0x1f)) + 63;
+			encodeString += (String.fromCharCode(nextValue));
+			num >>= 5;
+		}
+		finalValue = num + 63;
+		encodeString += (String.fromCharCode(finalValue));
+		return encodeString;
+	};
 
-	var encodeMixin = {
-		encodePath: function () {
-			return L.PolylineUtil.encode(this.getLatLngs());
+	// This one is Google's verbatim.
+	var encodeSignedNumber = function (num) {
+		var sgn_num = num << 1;
+		if (num < 0) {
+			sgn_num = ~(sgn_num);
+		}
+
+		return encodeNumber(sgn_num);
+	};
+
+	var getLat = function (latlng) {
+		if (latlng.lat) {
+			return latlng.lat;
+		} else {
+			return latlng[0];
+		}
+	};
+	var getLng = function (latlng) {
+		if (latlng.lng) {
+			return latlng.lng;
+		} else {
+			return latlng[1];
 		}
 	};
 
-	if (!L.Polyline.prototype.encodePath) {
-		L.Polyline.include(encodeMixin);
-	}
-	if (!L.Polygon.prototype.encodePath) {
-		L.Polygon.include(encodeMixin);
-	}
-})(L);
 
-// Utility functions.
-L.PolylineUtil = {};
+	var PolylineUtil = {
+		encode: function (latlngs) {
+			var i, dlat, dlng;
+			var plat = 0;
+			var plng = 0;
+			var encoded_points = "";
 
-L.PolylineUtil.encode = function (latlngs) {
-	var i, dlat, dlng;
-	var plat = 0;
-	var plng = 0;
-	var encoded_points = "";
+			for (i = 0; i < latlngs.length; i++) {
+				var lat = getLat(latlngs[i]);
+				var lng = getLng(latlngs[i]);
+				var late5 = Math.floor(lat * 1e5);
+				var lnge5 = Math.floor(lng * 1e5);
+				dlat = late5 - plat;
+				dlng = lnge5 - plng;
+				plat = late5;
+				plng = lnge5;
+				encoded_points += encodeSignedNumber(dlat) + encodeSignedNumber(dlng);
+			}
+			return encoded_points;
+		},
 
-	for (i = 0; i < latlngs.length; i++) {
-		var lat = latlngs[i].lat;
-		var lng = latlngs[i].lng;
-		var late5 = Math.floor(lat * 1e5);
-		var lnge5 = Math.floor(lng * 1e5);
-		dlat = late5 - plat;
-		dlng = lnge5 - plng;
-		plat = late5;
-		plng = lnge5;
-		encoded_points +=
-			L.PolylineUtil.encodeSignedNumber(dlat) +
-			L.PolylineUtil.encodeSignedNumber(dlng);
-	}
-	return encoded_points;
-};
+		decode: function (encoded) {
+			var len = encoded.length;
+			var index = 0;
+			var latlngs = [];
+			var lat = 0;
+			var lng = 0;
 
-// This function is very similar to Google's, but I added
-// some stuff to deal with the double slash issue.
-L.PolylineUtil.encodeNumber = function (num) {
-	var encodeString = "";
-	var nextValue, finalValue;
-	while (num >= 0x20) {
-		nextValue = (0x20 | (num & 0x1f)) + 63;
-		encodeString += (String.fromCharCode(nextValue));
-		num >>= 5;
-	}
-	finalValue = num + 63;
-	encodeString += (String.fromCharCode(finalValue));
-	return encodeString;
-};
+			while (index < len) {
+				var b;
+				var shift = 0;
+				var result = 0;
+				do {
+					b = encoded.charCodeAt(index++) - 63;
+					result |= (b & 0x1f) << shift;
+					shift += 5;
+				} while (b >= 0x20);
+				var dlat = ((result & 1) ? ~(result >> 1) : (result >> 1));
+				lat += dlat;
 
-// This one is Google's verbatim.
-L.PolylineUtil.encodeSignedNumber = function (num) {
-	var sgn_num = num << 1;
-	if (num < 0) {
-		sgn_num = ~(sgn_num);
-	}
-	return (L.PolylineUtil.encodeNumber(sgn_num));
-};
+				shift = 0;
+				result = 0;
+				do {
+					b = encoded.charCodeAt(index++) - 63;
+					result |= (b & 0x1f) << shift;
+					shift += 5;
+				} while (b >= 0x20);
+				var dlng = ((result & 1) ? ~(result >> 1) : (result >> 1));
+				lng += dlng;
 
-L.PolylineUtil.decode = function (encoded) {
-	var len = encoded.length;
-	var index = 0;
-	var latlngs = [];
-	var lat = 0;
-	var lng = 0;
+				latlngs.push([lat * 1e-5, lng * 1e-5]);
+			}
 
-	while (index < len) {
-		var b;
-		var shift = 0;
-		var result = 0;
-		do {
-			b = encoded.charCodeAt(index++) - 63;
-			result |= (b & 0x1f) << shift;
-			shift += 5;
-		} while (b >= 0x20);
-		var dlat = ((result & 1) ? ~(result >> 1) : (result >> 1));
-		lat += dlat;
+			return latlngs;
+		}
+	};
 
-		shift = 0;
-		result = 0;
-		do {
-			b = encoded.charCodeAt(index++) - 63;
-			result |= (b & 0x1f) << shift;
-			shift += 5;
-		} while (b >= 0x20);
-		var dlng = ((result & 1) ? ~(result >> 1) : (result >> 1));
-		lng += dlng;
-
-		latlngs.push(new L.LatLng(lat * 1e-5, lng * 1e-5));
+	// Export Node module
+	if (typeof module === 'object' && typeof module.exports === 'object') {
+		module.exports = PolylineUtil;
 	}
 
-	return latlngs;
-};
+	// Inject functionality into Leaflet
+	if (typeof L === 'object') {
+		if (!(L.Polyline.prototype.fromEncoded)) {
+			L.Polyline.fromEncoded = function (encoded, options) {
+				return new L.Polyline(PolylineUtil.decode(encoded), options);
+			};
+		}
+		if (!(L.Polygon.prototype.fromEncoded)) {
+			L.Polygon.fromEncoded = function (encoded, options) {
+				return new L.Polygon(PolylineUtil.decode(encoded), options);
+			};
+		}
+
+		var encodeMixin = {
+			encodePath: function () {
+				return PolylineUtil.encode(this.getLatLngs());
+			}
+		};
+
+		if (!L.Polyline.prototype.encodePath) {
+			L.Polyline.include(encodeMixin);
+		}
+		if (!L.Polygon.prototype.encodePath) {
+			L.Polygon.include(encodeMixin);
+		}
+
+		L.PolylineUtil = PolylineUtil;
+	}
+})();
---
+++ examples/node-example.js
@@ -0,0 +1,19 @@
+/* Use as a Nodejs module.
+ *
+ * Either require the file directly or use or use `npm install polyline-encoded` and require('polyline-encoded')
+ */
+var polyUtil = require('../Polyline.encoded.js');
+
+
+// encoding
+var latlngs = [
+	[38.5, -120.5],
+	[40.7, -120.95],
+	[43.252, -126.453]
+];
+// should print '_p~iF~cn~U_ulLn{vA_mqNvxq`@'
+console.dir(polyUtil.encode(latlngs));
+
+
+var encoded = '_p~iF~cn~U_ulLn{vA_mqNvxq`@';
+console.log(polyUtil.decode(encoded));
