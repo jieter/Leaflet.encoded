06db43d1d41470630b4cfd75f2573c9874b6cfed
Jan Pieter Waagmeester <jieter@jieter.nl>
2013-11-11 15:10:11 +0100

    clean up build script

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,45 @@
-var path = require('path');
-
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-notify');
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			options: {
+				'-W097': true
+			},
+			files: [
+				'*.js',
+				'examples/**/*.js',
+				'test/**/*.js',
+			]
+		},
 
-    jshint: {
-      options: {
-        '-W097': true
-      },
-      files: [
-        '*.js',
-        'examples/**/*.js',
-        'test/**/*.js',
-      ]
-    },
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec'
+				},
+				src: ['test/**/*.js']
+			}
+		},
 
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec'
-        },
-        src: ['test/**/*.js']
-      }
-    },
+		watch: {
+			scripts: {
+				files: [
+					'*.js',
+					'examples/**/*.js',
+					'test/**/*.js',
+				],
+				tasks: ['lint', 'test'],
+			},
+		}
+	});
 
-    watch: {
-      scripts: {
-        files: [
-          '*.js',
-          'examples/**/*.js',
-          'test/**/*.js',
-        ],
-        tasks: ['lint', 'test'],
-      },
-    }
-  });
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  // Default task(s).
-  grunt.registerTask('lint', ['jshint']);
-  grunt.registerTask('test', ['mochaTest']);
+	// Default task(s).
+	grunt.registerTask('lint', ['jshint']);
+	grunt.registerTask('test', ['mochaTest']);
 
-  grunt.registerTask('default', ['lint', 'test']);
-
+	grunt.registerTask('default', ['lint', 'test']);
 };
